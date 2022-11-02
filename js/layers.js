636f4afd211e4535689a0257ff9c3840851f7f0f
RTXT25 <skoolhumanboi@gmail.com>
2022-11-02 20:30:18 +1100

    SUSSy

---
+++ js/layers.js
@@ -0,0 +1,50 @@
+addLayer("d", {
+    name: "day", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "D", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#6AEC43",
+    requires: new Decimal(86400), // Can be a function that takes requirement increases into account
+    resource: "Days", // Name of prestige currency
+    baseResource: "Fun Points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "d", description: "d: Reset for days", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+
+    upgrades:{
+        11: {
+            title:"Half A Day",
+            cost: new Decimal(1),
+        },
+        12: {
+            title:"Half A Half A Day",
+            cost: new Decimal(1),
+            unlocked() { return hasUpgrade('d',11)},
+        },
+        13: {
+            title:"Half A Half A Half A Day",
+            cost: new Decimal(1),
+            unlocked() { return hasUpgrade('d',12)},
+        },
+        14: {
+            title:"1 HOUR????????",
+            cost: new Decimal(1),
+            unlocked() { return hasUpgrade('d',13)},
+        },
+    },
+    layerShown(){return true}
+})
