4eca99acf064ea3b848fe4ce36fb5c5de6d443e1
Nvidia RTXT2500 <78957603+RTXT25@users.noreply.github.com>
2022-11-11 09:25:57 +1100

    1.3

--- js/layers.js
+++ js/layers.js
@@ -11,7 +11,8 @@ addLayer("d", {
     resource: "Days", // Name of prestige currency
     baseResource: "Fun Points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    canbuymax() {return false},
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
